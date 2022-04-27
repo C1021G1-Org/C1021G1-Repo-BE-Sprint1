5b7d8cec834d9ab0ad13126e24e4aac5988fa680
LongTien <h2so4allii@gmail.com>
2022-04-27 18:39:12 +0700

    update

--- C1021G1_Sprint_1/src/main/java/com/codegym/controller/TicketController.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/controller/TicketController.java
@@ -1,8 +1,32 @@
 package com.codegym.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codegym.model.Ticket;
+import com.codegym.service.impl.TicketServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TicketController {
+
+    @Autowired
+    private TicketServiceImpl ticketService;
+
+    @RequestMapping(value = "/ticket/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Ticket> updateTicket(@PathVariable("id") Long id, @Param("buyer") String buyer , @Param("email_ticket") String email_ticket) {
+
+        Ticket ticket = ticketService.findTicketById(id);
+
+        if (ticket == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else {
+
+            ticketService.editTicket(id , buyer, email_ticket);
+            return new ResponseEntity<>(ticket, HttpStatus.OK);
+        }
+    }
+
 
 }
--- C1021G1_Sprint_1/src/main/java/com/codegym/repository/ITicketRepository.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/repository/ITicketRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ITicketRepository extends JpaRepository<Ticket, Long> {
-    @Query (value = "UPDATE Ticket set Ticket.buyer := buyer , Ticket.email_ticket:= email_ticket where id := ?", nativeQuery = true)
+    @Query (value = "UPDATE ticket set ticket.buyer := buyer , ticket.email_ticket:= email_ticket where id := ?", nativeQuery = true)
     void editTicket(Long id, @Param("buyer") String buyer  , @Param("email_ticket") String email_ticket);
 
 }

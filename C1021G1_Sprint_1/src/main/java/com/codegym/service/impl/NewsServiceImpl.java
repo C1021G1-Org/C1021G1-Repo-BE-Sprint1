06ea8fa369f9de4b1fa2b37c94678eca07b7ecac
datnvn <idolsupperman3215@gmail.com>
2022-04-27 22:51:43 +0700

    dat push code

--- C1021G1_Sprint_1/src/main/java/com/codegym/service/impl/NewsServiceImpl.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/service/impl/NewsServiceImpl.java
@@ -1,4 +1,33 @@
 package com.codegym.service.impl;
 
-public class NewsServiceImpl {
+import com.codegym.model.News;
+import com.codegym.repository.INewsRepository;
+import com.codegym.service.INewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class NewsServiceImpl implements INewsService {
+    @Autowired
+    private INewsRepository iNewsRepository;
+
+    @Override
+    public Page<News> findAllNews(Pageable pageable) {
+        return iNewsRepository.findAllNews(pageable);
+    }
+
+    @Override
+    public void deleteNewsById(Long id) {
+        iNewsRepository.removeNewsById(id);
+    }
+
+    @Override
+    public Optional<News> findNewsById(Long id) {
+        return iNewsRepository.findNewsById(id);
+    }
+
 }

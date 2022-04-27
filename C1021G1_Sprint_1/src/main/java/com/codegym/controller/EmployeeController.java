886f1f9f40a6ea141b54c150d1a119fb830e325c
WiLynd <hdqphu.dev@gmail.com>
2022-04-27 16:32:06 +0700

    push-PhuHDQ-list-delete

--- C1021G1_Sprint_1/src/main/java/com/codegym/controller/EmployeeController.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/controller/EmployeeController.java
@@ -1,8 +1,53 @@
 package com.codegym.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codegym.model.Employee;
+import com.codegym.model.EmployeeType;
+import com.codegym.service.IEmployeeService;
+import com.codegym.service.IEmployeeTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 
+    @Autowired
+    private IEmployeeService iEmployeeService;
+
+    @Autowired
+    private IEmployeeTypeService iEmployeeTypeService;
+
+    @GetMapping("")
+    public ResponseEntity<Page<Employee>> getAllEmployee(@RequestParam(defaultValue = "0") int page, Pageable pageable){
+        Page<Employee> employees = iEmployeeService.findAllEmployee(PageRequest.of(page,10), pageable);
+        if (employees.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/employeeType")
+    public ResponseEntity<List<EmployeeType>> getAllEmployeeType() {
+        List<EmployeeType> employeeTypes = iEmployeeTypeService.findAllEmployeeType();
+        if (employeeTypes.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(employeeTypes, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable Long id) {
+        Optional<Employee> employee = iEmployeeService.findEmployeeById(id);
+        if (employee.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        iEmployeeService.deleteEmployee(id);
+        return new ResponseEntity<>(employee.get(), HttpStatus.NO_CONTENT);
+    }
 }
--- C1021G1_Sprint_1/src/main/java/com/codegym/service/impl/EmployeeServiceImpl.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/service/impl/EmployeeServiceImpl.java
@@ -1,4 +1,34 @@
 package com.codegym.service.impl;
 
-public class EmployeeServiceImpl {
+import com.codegym.model.Employee;
+import com.codegym.repository.IEmployeeRepository;
+import com.codegym.service.IEmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements IEmployeeService {
+
+    @Autowired
+    IEmployeeRepository iEmployeeRepository;
+
+    @Override
+    public Page<Employee> findAllEmployee(PageRequest of, Pageable pageable) {
+        return iEmployeeRepository.findAllEmployee(of, pageable);
+    }
+
+    @Override
+    public Optional<Employee> findEmployeeById(Long id) {
+        return iEmployeeRepository.findEmployeeById(id);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        iEmployeeRepository.deleteEmployee(id);
+    }
 }

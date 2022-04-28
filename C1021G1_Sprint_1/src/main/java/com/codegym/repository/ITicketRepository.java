4538b613a0079fa0aa27d2b9898f42fc44256f80
unknown <you@example.com>
2022-04-28 13:38:18 +0700

    pull

--- C1021G1_Sprint_1/src/main/java/com/codegym/repository/ITicketRepository.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/repository/ITicketRepository.java
@@ -1,10 +1,20 @@
 package com.codegym.repository;
 
+import com.codegym.model.Customer;
 import com.codegym.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Repository
+
 public interface ITicketRepository extends JpaRepository<Ticket,Long> {
+
+
+
 
 }

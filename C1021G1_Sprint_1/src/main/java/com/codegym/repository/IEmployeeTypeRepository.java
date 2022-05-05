3a90da7f1ab188f9eba1beebd671795cc781d44f
WiLynd <hdqphu.dev@gmail.com>
2022-05-05 15:32:22 +0700

    push-BE

--- C1021G1_Sprint_1/src/main/java/com/codegym/repository/IEmployeeTypeRepository.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/repository/IEmployeeTypeRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface IEmployeeTypeRepository extends JpaRepository<EmployeeType, Long> {
-
     @Query(value = "SELECT id, name_employee_type FROM employee_type",nativeQuery = true)
     List<EmployeeType> findAllEmployeeType();
 }

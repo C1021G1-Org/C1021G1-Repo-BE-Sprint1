9efab5c8490104ecdfb5e49fbd1355bb985e7f8c
huynhtrong1711 <trongoliver1998@gmail.com>
2022-05-05 09:33:29 +0700

    trong pull

--- C1021G1_Sprint_1/src/main/java/com/codegym/service/IFlightService.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/service/IFlightService.java
@@ -2,8 +2,10 @@ package com.codegym.service;
 
 import com.codegym.dto.FlightDto;
 import com.codegym.model.Flight;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import java.util.Map;
 
-import java.util.List;
 
 public interface IFlightService {
 
@@ -13,18 +15,11 @@ public interface IFlightService {
 
     void updateFlight(FlightDto flightDto);
 
-import com.codegym.dto.FlightDto;
-import com.codegym.model.Flight;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import java.util.Map;
-
-public interface IFlightService {
     Map<String, Page<FlightDto>> searchFlight(String departureDestination, String arrivalDestination, String departureDate, String arrivalDate, Pageable pageable);
 
     Page<Flight> findAllFlight(Pageable pageable);
 
-    Flight findById(Long id);
+    Flight findById1(Long id);
 
     void deleteById(Long id);
 }

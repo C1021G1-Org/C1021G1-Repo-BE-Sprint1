26551224b21cd98afe0dfd8d99e63587ea76f33b
Ho Nhat Hieu <honhathieu97@gmail.com>
2022-05-05 09:28:53 +0700

    hieuhn push flight

--- C1021G1_Sprint_1/src/main/java/com/codegym/controller/FlightController.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/controller/FlightController.java
@@ -10,48 +10,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
-
-import com.codegym.dto.FlightDto;
 import com.codegym.model.AirlineType;
-import com.codegym.model.Flight;
 import com.codegym.service.IAirlineTypeService;
-
-
-import com.codegym.model.FormSearch;
-
-import com.codegym.service.IFlightService;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-
-import org.springframework.data.domain.PageRequest;
-
-
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-
-import java.util.List;
-import java.util.Optional;
-
-
-
-
-
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/flight")
-
-
 public class FlightController {
     @Autowired
     private IFlightService flightService;

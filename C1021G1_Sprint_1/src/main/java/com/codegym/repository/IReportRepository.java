4a42a57ae8c72a1f3267d6787ca062dfd623eb9a
thuan <you@example.com>
2022-05-06 21:47:37 +0700

    thuanpd

--- C1021G1_Sprint_1/src/main/java/com/codegym/repository/IReportRepository.java
+++ C1021G1_Sprint_1/src/main/java/com/codegym/repository/IReportRepository.java
@@ -28,24 +28,38 @@ public interface IReportRepository extends JpaRepository<Ticket,Long> {
 //         " ORDER BY MONTH(date_start);",nativeQuery = true)
 //    List<IReport> getAllReport();
 
-    @Query(value = " SELECT SUM(price_ticket)totalPrice,COUNT(ticket.point_ticket)poinTicket,MONTH(date_start)monthStartDate FROM seat\n" +
-            " INNER JOIN seat_type ON seat.id_seat_type = seat_type.id \n" +
-            " INNER JOIN flight ON seat.id_flight = flight.id \n" +
-            " INNER JOIN ticket ON ticket.id_seat = ticket.id\n" +
-            " where MONTH(date_start)\n" +
-            " GROUP BY MONTH(date_start)\n" +
-            " ORDER BY MONTH(date_start); ",nativeQuery = true)
+    @Query(value = " SELECT SUM(price_ticket)totalPrice," +
+            "COUNT(ticket.point_ticket)poinTicket," +
+            "MONTH(date_start)monthStartDate " +
+            "FROM seat\n" +
+            "INNER JOIN seat_type " +
+            "ON seat.id_seat_type = seat_type.id \n" +
+            "INNER JOIN flight " +
+            "ON seat.id_flight = flight.id \n" +
+            "INNER JOIN ticket " +
+            "ON ticket.id_seat = ticket.id\n" +
+            "WHERE (MONTH(date_start) BETWEEN 1 AND 12)\n" +
+            "GROUP BY MONTH(date_start)\n" +
+            "ORDER BY MONTH(date_start); ",nativeQuery = true)
     List<IReport> getAllReport();
 
 
 
-    @Query(value = "SELECT employee.id, employee.name_employee,SUM(ticket.point_ticket)sumPoint,MONTH(date_start)monthEmployee \n" +
+    @Query(value = "SELECT employee.id, employee.name_employee," +
+            "SUM(ticket.point_ticket)sumPoint," +
+            "MONTH(date_start)monthEmployee \n" +
             "FROM employee\n" +
-            "INNER JOIN ticket ON ticket.id_employee = employee.id\n" +
-            "INNER JOIN seat ON seat.id = ticket.id_seat\n" +
-            "INNER JOIN flight  ON seat.id_flight = seat.id\n" +
-            "WHERE status_ticket = true AND ticket.email_ticket = employee.email_employee \n" +
-            "GROUP BY employee.id ORDER BY SUM(ticket.point_ticket) DESC;",nativeQuery = true)
+            "INNER JOIN ticket " +
+            "ON ticket.id_employee = employee.id\n" +
+            "INNER JOIN seat " +
+            "ON seat.id = ticket.id_seat\n" +
+            "INNER JOIN flight  " +
+            "ON seat.id_flight = seat.id\n" +
+            "WHERE status_ticket = true  " +
+            "AND ticket.buyer_ticket = 'Nhân Viên'" +
+            "AND ticket.email_ticket = employee.email_employee \n" +
+            "GROUP BY employee.id " +
+            "ORDER BY SUM(ticket.point_ticket) DESC;",nativeQuery = true)
     List<IReportEmployee> getAllReportEmployee();
 
     @Query(value = "SELECT  airline_type.name_airline," +
@@ -59,7 +73,7 @@ public interface IReportRepository extends JpaRepository<Ticket,Long> {
             "ON ticket.id_seat = seat.id\n" +
             "WHERE (date_start  BETWEEN ?1 AND ?2) \n" +
             "GROUP BY airline_type.id " +
-            "ORDER BY COUNT(flight.id_airline_type) DESC;",nativeQuery = true)
+            "ORDER BY COUNT(flight.id_airline_type) ASC;",nativeQuery = true)
     List<IReportAirlineType> getAllAirlineType(String fromDate,String toDate);
 
 
